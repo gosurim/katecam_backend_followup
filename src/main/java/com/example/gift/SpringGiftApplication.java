09ae277ec9e18df8ff34bbf80ea785bf4f163043
gosurim <su020900@gmail.com>
2025-07-01 19:58:53 +0900

    init: Spring boot 프로젝트 초기 설정 및 의존성 추가
    의존성: Spring WEB, JDBC, H2, Thymeleaf

---
+++ src/main/java/com/example/gift/SpringGiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.gift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gift/SpringGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
